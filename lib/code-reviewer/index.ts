6ef7a11c9b35fd797f13eabc05327c7038db12af
Piotr Grzybowski <grzybowski_p@hotmail.com>
2025-06-24 12:57:19 +0200

    feat: implement code review workflow with OpenAI integration

---
+++ lib/code-reviewer/index.ts
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import {readFileSync} from "fs";
+import {commentOnPullRequest} from "./src/github-client.js";
+import {conductCodeReview} from "./src/openai-clinet.js";
+
+const config = {
+  openApiKey: process.env["OPENAI_API_KEY"],
+  githubKey: process.env["GITHUB_TOKEN"],
+  repoNameOwner: process.env["GITHUB_REPO_NAME_OWNER"],
+  issueNo: parseInt(process.env["ISSUE_NUMBER"] as string),
+};
+
+async function main() {
+  if (!config.openApiKey || !config.githubKey || !config.repoNameOwner) {
+    throw new Error(
+      "Missing env varables. API kesy & repo metadata must be provided."
+    );
+  }
+
+  const diffContent = readFileSync("./diff.txt", "utf-8");
+  const modelResponse = await conductCodeReview(config.openApiKey, diffContent);
+
+  await commentOnPullRequest({
+    githubKey: config.githubKey,
+    repoNameOwner: config.repoNameOwner,
+    issueNo: config.issueNo,
+    body: modelResponse,
+  });
+}
+
+main();
